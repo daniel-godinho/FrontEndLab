9637b881644b60aef832985505a2117de996e6cf
Daniel Godinho <danielgodinho772@gmail.com>
2026-08-06 13:55:49 -0300

    fix(interactive forms): fix transitions between login and sign-up

--- interactive-forms/script.js
+++ interactive-forms/script.js
@@ -1,13 +1,13 @@
 let card = document.querySelector('.card');
 let loginButton = document.querySelector('.loginButton');
-let BtnRegister = document.querySelector('.BtnRegister');
+let btnRegister = document.querySelector('.btnRegister');
 
 loginButton.onclick = () => {
   card.classList.remove('cadastroActive');
   card.classList.add('loginActive');
 };
 
-loginButton.onclick = () => {
+btnRegister.onclick = () => {
   card.classList.remove('loginActive');
   card.classList.add('cadastroActive');
 };
